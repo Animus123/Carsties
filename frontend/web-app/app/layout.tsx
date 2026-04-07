69fc6c030161397527c4264c7e9baa380a6db0da
Сергей Зыков <cybergoldfinch@gmail.com>
2026-04-07 14:26:48 +0400

    End of section 8

---
+++ frontend/web-app/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import NavBar from "./Nav/NavBar";
+
+export const metadata: Metadata = {
+  title: "Carsties",
+  description: "Carsties",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+            <body>
+              <NavBar />
+              <main className="container mx-auto px-5 pt-10">
+                {children}
+              </main>
+            </body>
+    </html>
+  );
+}
